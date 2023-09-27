3dcd822e94af664f50792533e78b13bc45e855a4
Umaima Yousuf <112200777+Umaima1205@users.noreply.github.com>
2023-09-27 20:12:25 +0500

    Add project files.

---
+++ ASSIGNMENT#3/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+           
+
+namespace ASSIGNMENT_3
+    {
+        internal class Program
+        {
+            static void Main(string[] args)
+            {
+                Console.WriteLine("Expense Tracker");
+                Console.WriteLine("Press Enter To Type The Name Of An Expense");
+                Console.ReadKey(); 
+
+                Console.WriteLine("Type The Name Of An Expense");
+                string Name = Console.ReadLine(); 
+
+                Console.WriteLine("Press Enter To Type The Cost Of An Expense");
+                Console.ReadKey(); 
+
+                Console.WriteLine("Type The Cost Of An Expense");
+                string Cost = Console.ReadLine(); 
+
+                Console.WriteLine("The Expense Name Is : " + Name);
+                Console.WriteLine("The Cost Of An Expense Is : " + Cost);
+                Console.ReadLine(); 
+            }
+        }
+    }
+
+
